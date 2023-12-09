705f66f33180c676c5a50276a2239bb619b71cf6
MohammedGitIt <mohammedunkown105@gmail.com>
2023-12-09 16:37:46 +0100

    chaning db

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,8 @@
 import express, { json, Request } from "express";
-import db from "./services/db-connection.ts";
+// import db from "./services/db-connection.js";
 import { Query } from "mysql2";
 import cors from "cors";
+import { client } from "./services/db-connection.js";
 
 let app = express();
 
@@ -13,44 +14,39 @@ app.use(
   })
 );
 
-app.get("/api/city", async (req: Request, res) => {
-  console.log(req.query.champ);
-  const query = db.query(
-    "SELECT name FROM city where name LIKE '%k%' limit 10",
-    (err, results, fields) => {
-      res.send(results);
-      console.log(fields);
-      console.log(err);
-    }
-  );
+// app.get("/api/city", async (req: Request, res) => {
+//   console.log(req.query.champ);
+//   const query = db.query(
+//     "SELECT name FROM city where name LIKE '%k%' limit 10",
+//     (err, results, fields) => {
+//       res.send(results);
+//       console.log(fields);
+//       console.log(err);
+//     }
+//   );
 
-  query.on("end", () => console.log("done"));
-});
+//   query.on("end", () => console.log("done"));
+// });
 
 app.post("/api/saveToDo", async (req: Request, res) => {
   console.log(req.body);
-  const query = db.execute(
-    "insert into todos (id,todo) values (null,?)",
-    [req.body.todo],
-    (err, results, fields) => {
-      res.send(results);
-      console.log(fields);
-      console.log(err);
-    }
+  await client.connect();
+  const query = await client.query(
+    "insert into todos (id,todo) values (null,$1)",
+    [req.body.todo]
   );
-  query.on("end", () => console.log("done"));
+
+  res.send(query.fields);
 });
 
 app.get("/api/todos", async (req: Request, res) => {
-  console.log(req.query.champ);
-  const query = db.query("SELECT * FROM todos ", (err, results, fields) => {
-    res.send(results);
-    console.log(fields);
-    console.log(err);
-  });
+  await client.connect();
+  const query = await client.query("SELECT * FROM todos");
 
-  query.on("end", () => console.log("done"));
+  res.send(query.fields);
+  console.log(query.rows[0]);
 });
-app.listen(3000, () => console.log("opened server onport 3000..."));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("opened server onport 3000..."));
 
 export default app;
--- server/src/services/db-connection.ts
+++ server/src/services/db-connection.ts
@@ -1,5 +1,6 @@
 import { Connection, createConnection } from "mysql2";
 // import mysql from 'mysql2/promise';
+import { Client } from "pg";
 
 const connection: Connection = createConnection({
   host: "localhost",
@@ -8,5 +9,7 @@ const connection: Connection = createConnection({
   port: 3305,
   password: "root",
 });
+
+export const client = new Client({ ssl: true });
 
 export default connection;
